87dcf3585e7a2e64fd836390b7221d82676df450
Kivikoura <matias.kivikoura@gmail.com>
2016-01-29 22:21:14 +0200

    Hahmoilla omat ohjaukset

--- Assets/Scripts/EasyMove.cs
+++ Assets/Scripts/EasyMove.cs
@@ -1,33 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EasyMove : MonoBehaviour {
+public class EasyMove : MonoBehaviour
+{
 
+    public int ControlMode = 0;
 	public float Speed = 0f;
 	private float movex = 0f;
 	private float movey = 0f;
 
+    private enum dir
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey (KeyCode.A))
+		if (Input.GetKey (getKeyCode(dir.LEFT)))
 			movex = -1;
-		else if (Input.GetKey (KeyCode.D))
+		else if (Input.GetKey (getKeyCode(dir.LEFT)))
 			movex = 1;
 		else
 			movex = 0;
-		if (Input.GetKey (KeyCode.W))
+		if (Input.GetKey (getKeyCode(dir.UP)))
 			movey = 1;
-		else if (Input.GetKey (KeyCode.S))
+		else if (Input.GetKey (getKeyCode(dir.DOWN)))
 			movey = -1;
 		else
 			movey = 0;
 	}
+
+    KeyCode getKeyCode(dir direction)
+    {
+        switch (ControlMode)
+        {
+            case 0:
+                if (direction == dir.UP) return KeyCode.W;
+                if (direction == dir.RIGHT) return KeyCode.D;
+                if (direction == dir.DOWN) return KeyCode.S;
+                if (direction == dir.LEFT) return KeyCode.A;
+                break;
+            case 1:
+                if (direction == dir.UP) return KeyCode.UpArrow;
+                if (direction == dir.RIGHT) return KeyCode.RightArrow;
+                if (direction == dir.DOWN) return KeyCode.DownArrow;
+                if (direction == dir.LEFT) return KeyCode.LeftArrow;
+                break;
+            default:
+                if (direction == dir.UP) return KeyCode.W;
+                if (direction == dir.RIGHT) return KeyCode.D;
+                if (direction == dir.DOWN) return KeyCode.S;
+                if (direction == dir.LEFT) return KeyCode.A;
+                break;
+        }
+        return KeyCode.None;
+    }
 
 	void FixedUpdate ()
 	{
