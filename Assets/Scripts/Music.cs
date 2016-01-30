0a9e06aec44998a3a78de3557920e63b0bd57757
Kivikoura <matias.kivikoura@gmail.com>
2016-01-31 01:44:22 +0200

    Musa

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -5,11 +5,13 @@ public class Music : MonoBehaviour
 {
 
     public AudioClip[] AudioClips;
+    public AudioSource Source;
 
 	// Use this for initialization
 	void Start ()
-    {
-	    
+	{
+	    Source.clip = AudioClips[0];
+        Source.Play();
 	}
 	
 	// Update is called once per frame
