56178ee2d5ae34fcacd58730ec817e1b02b77db7
Kivikoura <matias.kivikoura@gmail.com>
2016-01-31 14:54:04 +0200

    NextVerse siirtää seuraavaan biisiin

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -6,19 +6,33 @@ public class Music : MonoBehaviour
 
     public AudioClip[] AudioClips;
     public AudioSource Source;
+    private int current;
+    public bool next;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    Source.clip = AudioClips[0];
+	    next = false;
+	    current = 0;
+	    Source.clip = AudioClips[current];
         Source.Play();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    
+	    if (next && Source.time > 23.99)
+	    {
+	        Source.clip = AudioClips[++current];
+            Source.Play();
+	        next = false;
+	    }
 	}
+
+    void NextVerse()
+    {
+        next = true;
+    }
 
 
 }
